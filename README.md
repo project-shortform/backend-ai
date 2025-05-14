d68ac3179cb5b465f9841a2bdd9905247d6ac9c0
hyeonho cho <111675679+aesthetics-of-record@users.noreply.github.com>
2025-05-14 14:12:30 +0900

    refactor

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 ```bash
 uv run main.py
 ```
+
+# 프로젝트 swagger 접속
+```bash
+http://localhost:5000/docs
+```
+
--- src/routers/edit.py
+++ src/routers/edit.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter
+from moviepy import VideoFileClip
 
 
 router = APIRouter(prefix="/api/edit")
-
 
 @router.post("/edit")
 def edit_video(text: str):
